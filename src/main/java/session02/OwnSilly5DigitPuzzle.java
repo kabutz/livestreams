7625719f38c1bbdce08e3b16baea89c470b824da
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-07-15 14:26:06 +0300

    Updated to fit with Pierre Laporte's solution ;-)

--- src/main/java/session02/OwnSilly5DigitPuzzle.java
+++ src/main/java/session02/OwnSilly5DigitPuzzle.java
@@ -7,7 +7,7 @@ import static session02.SillyNumberPuzzle.*;
 
 public class OwnSilly5DigitPuzzle {
   public static void main(String... args) {
-    int[] correct = {4, 4, 6, 3, 4};
+    int[] correct = {4, 4, 4, 3, 5};
 
     for (int guess0 = 0; guess0 <= 9; guess0++) {
       for (int guess1 = 0; guess1 <= 9; guess1++) {
@@ -18,8 +18,8 @@ public class OwnSilly5DigitPuzzle {
               if (true
                   && check(guesses, 1, 1, 6, 5, 0, 3, 2) // 65032 - Two digits are right, one in its place and one in the wrong place
                   && check(guesses, 0, 1, 1, 8, 6, 9, 5) // 18695 - One digits is right, and in its place
-                  && check(guesses, 0, 0, 0, 2, 1, 8, 7) // 98721 - All digits are wrong
-                  && check(guesses, 1, 0, 1, 3, 5, 7, 9) // 13579 - Two digits are right, but in the wrong place
+                  && check(guesses, 0, 0, 9, 8, 7, 2, 1) // 98721 - All digits are wrong
+                  && check(guesses, 2, 0, 1, 3, 5, 7, 9) // 13579 - Two digits are right, but in the wrong place
               ) {
                 System.out.println(Arrays.toString(guesses));
               }
