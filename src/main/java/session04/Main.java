e6d972af8e8ddb8b1f9794be49117e1b52ee9e5b
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-08-12 10:21:42 +0300

    Latest ping results

--- src/main/java/session04/Main.java
+++ src/main/java/session04/Main.java
@@ -5,14 +5,14 @@ import java.net.*;
 
 public class Main {
   public static void main(String... args) throws IOException, URISyntaxException {
-    PingAnalyzer analyzer = new PingAnalyzer("session04/pings-2020-07-22.txt");
+    PingAnalyzer analyzer = new PingAnalyzer("session04/pings-2020-08-09-hexie.txt");
 
     System.out.println("All results:");
     analyzer.getPingResults().forEach(System.out::println);
 
     System.out.println();
     System.out.println("Merged results:");
-    analyzer.getMergedPingResults(3).forEach(System.out::println);
+    analyzer.getMergedPingResults(10).forEach(System.out::println);
 
     System.out.println();
     System.out.println("Total timeouts: " + analyzer.getTotalNumberOfTimeouts());
