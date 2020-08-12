48fa8c93a11368b7cb2993968058b7e1f6d5532d
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2020-08-12 11:11:23 +0300

    computeIfAbsent

--- src/main/java/session06/MapInverter.java
+++ src/main/java/session06/MapInverter.java
@@ -6,12 +6,8 @@ public class MapInverter {
   public static <K, V> Map<V, List<K>> invert(Map<K, V> map) {
     Map<V, List<K>> result = new HashMap<>();
     for (Map.Entry<K, V> entry : map.entrySet()) {
-      List<K> list = result.get(entry.getValue());
-      if (list == null) {
-        list = new ArrayList<>();
-        result.put(entry.getValue(), list);
-      }
-      list.add(entry.getKey());
+      result.computeIfAbsent(entry.getValue(),
+          v -> new ArrayList<>()).add(entry.getKey());
     }
     return result;
   }
